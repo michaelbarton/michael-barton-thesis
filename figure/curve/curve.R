376e6e05759b52a0efcfa4390b31d7eb8d470465
Michael Barton <mail@michaelbarton.me.uk>
2008-09-11 18:36:56 +0100

    Added r script to produce curve figure

--- figure/curve/curve.R
+++ figure/curve/curve.R
@@ -11,15 +11,17 @@ for(i in 1:length(envs)) {
   
 }
 
-lines = c(2,3)
+lines = c(1,2)
 
 nit = subset = data[data$env == 'nitrogen',]
 glu = subset = data[data$env == 'glucose',]
 
+postscript("curve.eps",width=5,height=5,onefile=FALSE,horizontal=FALSE, paper = "special",colormodel="rgb")
+
 plot(biomass ~ change,data=data,type='n')
-lines(biomass ~ change, data=glu,lwd=2,lty=lines[1],col="grey10")
-lines(biomass ~ change, data=nit,lwd=2,lty=lines[2],col="grey10")
+lines(biomass ~ change, data=glu,lwd=2,lty=lines[1],col="grey20")
+lines(biomass ~ change, data=nit,lwd=2,lty=lines[2],col="grey20")
+legend("topleft",legend=envs,lty=lines,col="grey20")
 
-legend("topleft",legend=envs,lty=lines)
-
+graphics.off()
 
